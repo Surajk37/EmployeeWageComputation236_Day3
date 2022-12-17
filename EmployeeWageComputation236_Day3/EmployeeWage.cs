af3c8b891a04457722183f2ccf7a825b85cf06d0
Surajk37 <surajkhot9993@gmail.com>
2022-12-17 13:35:37 +0530

    [Suraj] : Welcome Message Added

---
+++ EmployeeWageComputation236_Day3/EmployeeWage.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWageComputation236_Day3
+{
+    class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\t\t Welcome To Employee Wage Computation Problem\n");
+            Console.ReadLine();
+        
+        }
+    }
+}
