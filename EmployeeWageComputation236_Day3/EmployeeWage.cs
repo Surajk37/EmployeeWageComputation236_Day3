913cbecd51e2e65a2ff47a1d3666488d9a475db8
Surajk37 <surajkhot9993@gmail.com>
2022-12-17 13:39:46 +0530

    [Suraj] Added Employee Attendance Program

--- EmployeeWageComputation236_Day3/EmployeeWage.cs
+++ EmployeeWageComputation236_Day3/EmployeeWage.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
+            const int IS_PRESENT = 0;
+            Console.WriteLine("\t\tWelcome To Employee Wage Computation Problem\n");
 
-            Console.WriteLine("\t\t Welcome To Employee Wage Computation Problem\n");
+            //UC1 Check employee is present or absent
+            Random random = new Random();
+            int Input = random.Next(0, 2);
+
+            if (IS_PRESENT == Input)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
             Console.ReadLine();
-        
         }
     }
 }
