6c97c6388b3be7ede6730ed1a436c1c24dd16e76
Surajk37 <surajkhot9993@gmail.com>
2022-12-17 14:43:42 +0530

    Calculated Employee Wage according to Time

--- EmployeeWageComputation236_Day3/EmployeeWage.cs
+++ EmployeeWageComputation236_Day3/EmployeeWage.cs
@@ -4,23 +4,36 @@
     {
         static void Main(string[] args)
         {
+            const int Is_Full_Time = 0;
+            const int Is_Part_Time = 1;
             Console.WriteLine("\t\tWelcome To Employee Wage Computation Problem\n");
-            int Is_Full_Time = 1;
             int Emp_Rate_PerHr = 20;
-
             int empHrs = 0;
             int empWages = 0;
 
-            //UC2 Calculating Daily Employee Wage
+            //UC3 Calculating Employee Wage Based On Time
             Random random = new Random();
-            int Input = random.Next(0, 2);
+            int Input = random.Next(0, 3);
 
             if (Is_Full_Time == Input)
+            {
                 empHrs = 8;
+                Console.WriteLine("\nEmployee is Present and He his Full Time");
+
+            }
+            else if (Is_Part_Time == Input)
+            {
+                empHrs = 4;
+
+                Console.WriteLine("\nEmployee is Present and He his Part Time");
+            }
             else
+            {
                 empHrs = 0;
-            empWages = empHrs * Emp_Rate_PerHr;
-            Console.WriteLine("Employee Daily Wages are : " + empWages);
+                Console.WriteLine("Employee is Absent");
+            }
+                empWages = empHrs * Emp_Rate_PerHr;
+            Console.WriteLine("Employee Wages are : " + empWages);
             Console.ReadLine();
         }
     }
