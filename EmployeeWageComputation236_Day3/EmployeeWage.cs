8f43de2b3d29e47f480920fc25de92373f2c624f
Surajk37 <surajkhot9993@gmail.com>
2022-12-17 14:07:15 +0530

    [Suraj] Calculated Daily Employee Wage

--- EmployeeWageComputation236_Day3/EmployeeWage.cs
+++ EmployeeWageComputation236_Day3/EmployeeWage.cs
@@ -4,17 +4,23 @@
     {
         static void Main(string[] args)
         {
-            const int IS_PRESENT = 0;
             Console.WriteLine("\t\tWelcome To Employee Wage Computation Problem\n");
+            int Is_Full_Time = 1;
+            int Emp_Rate_PerHr = 20;
 
-            //UC1 Check employee is present or absent
+            int empHrs = 0;
+            int empWages = 0;
+
+            //UC2 Calculating Daily Employee Wage
             Random random = new Random();
             int Input = random.Next(0, 2);
 
-            if (IS_PRESENT == Input)
-                Console.WriteLine("Employee is Present");
+            if (Is_Full_Time == Input)
+                empHrs = 8;
             else
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            empWages = empHrs * Emp_Rate_PerHr;
+            Console.WriteLine("Employee Daily Wages are : " + empWages);
             Console.ReadLine();
         }
     }
