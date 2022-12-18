e81846c051d684f866d33c2434c74abcd9876e3e
Surajk37 <surajkhot9993@gmail.com>
2022-12-18 21:13:32 +0530

    [Suraj] Uploaded Abstraction Concept

--- EmployeeWageComputation236_Day3/EmployeeWage.cs
+++ EmployeeWageComputation236_Day3/EmployeeWage.cs
@@ -4,35 +4,33 @@
     {
         static void Main(string[] args)
         {
-            const int Is_Full_Time = 0;
-            const int Is_Part_Time = 1;
             Console.WriteLine("\t\tWelcome To Employee Wage Computation Problem\n");
             int Emp_Rate_PerHr = 20;
             int empHrs = 0;
             int empWages = 0;
 
-            //UC3 Calculating Employee Wage Based On Time
+            //UC4 Calculating Employee Wage Based On Time using Swtich case
             Random random = new Random();
             int Input = random.Next(0, 3);
 
-            if (Is_Full_Time == Input)
+            switch (Input)
             {
-                empHrs = 8;
-                Console.WriteLine("\nEmployee is Present and He his Full Time");
+                case 1:
+                    empHrs = 8;
+                    Console.WriteLine("\nEmployee is Present and He his Full Time");
+                    break;
 
+                case 2:
+                    empHrs = 4;
+                    Console.WriteLine("\nEmployee is Present and He his Part Time");
+                    break;
+                
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
-            else if (Is_Part_Time == Input)
-            {
-                empHrs = 4;
-
-                Console.WriteLine("\nEmployee is Present and He his Part Time");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
-            }
-                empWages = empHrs * Emp_Rate_PerHr;
+            empWages = empHrs * Emp_Rate_PerHr;
             Console.WriteLine("Employee Wages are : " + empWages);
             Console.ReadLine();
         }
